6c207ef790d4a90ea105e234fe6468b0c94c4ecd
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-14 15:32:08 +0530

    Assignment done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    return list.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
