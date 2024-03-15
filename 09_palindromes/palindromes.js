3637f71a07ac28cab199aa0cd364c339b4805654
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-15 16:25:12 +0530

    implemented punctuation checker and whitespace removal

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    let filtered = arr.filter(notpunctuation);
+    let letterArray = charactersToList(filtered);
+    
+
 
 };
+
+function notpunctuation(text) {
+    const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    return !punctuationRegex.test(text);
+};
+
+function charactersToList(str) {
+    // Initialize an empty list to store characters
+    var charList = [];
+
+    // Iterate over each character in the string
+    for (var i = 0; i < str.length; i++) {
+        // Check if the character is not a space
+        if (str[i] !== ' ') {
+            // Add the character to the list
+            charList.push(str[i]);
+        }
+    }
+
+    // Return the list of characters
+    return charList;
 
 // Do not edit below this line
 module.exports = palindromes;
