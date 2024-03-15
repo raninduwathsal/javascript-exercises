12d97f6d1758e95ba49205001baf00f9fcad6c6a
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-15 18:12:35 +0530

    assignment done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+      const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
+  };
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
 };
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
