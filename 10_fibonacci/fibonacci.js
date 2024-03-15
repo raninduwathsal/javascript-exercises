4536c2ecdfe76e4456861777a6d0c4e697cd6705
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-15 17:04:42 +0530

    excersise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+        // Convert n to integer if it's a string
+        if (typeof n === 'string') {
+            n = parseInt(n);
+        }
+    
+        // Check if n is not a number or is less than 0
+        if (isNaN(n) || n < 0) {
+            return "OOPS"
+        }
+    
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
 
 };
 
