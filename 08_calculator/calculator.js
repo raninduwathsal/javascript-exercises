feebbe19bf423a248fad0d8b6f9dd5c4f4022da9
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-15 16:16:01 +0530

    assigment done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+const sum = function(arr) {
+  // Check if the array is empty
+  if (arr.length === 0) {
+    return 0; // Return 0 if the array is empty
+}
+// Reduce the array to get the sum
+return arr.reduce((sum, current) => sum + current);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum *  current);
+
+};
+
+const power = function(num1,num2) {
+  // return arr.reduce((sum, current) => sum ** current);
+  return num1 ** num2;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num1) {
+  if (num1 === 0){
+    return 1;
+  }
+  let arr = Array.from({ length: num1 }, (_, i) => i + 1);
+  return arr.reduce((sum, current) => sum *  current);
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
