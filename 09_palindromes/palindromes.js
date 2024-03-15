3a4dab3a2ce62b450cdbaeca6ec1b5b917ef1fb2
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-15 16:46:13 +0530

    assignment done with cleaner code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,35 +1,12 @@
-const palindromes = function (arr) {
-    let letterArrayUnfiltered = charactersToList(arr);
-    let letterArray = letterArrayUnfiltered.filter(ispunctuation);
-    let revesedArray = letterArray.reverse();
-    if (revesedArray === letterArray){
-        return true;
-    }
-    return false;
-};
-
-function ispunctuation(text) {
-    if (text === "!" || text === "," || text ==="."){
-        return false;
-    }
-    true;
-};
-
-function charactersToList(str) {
-    // Initialize an empty list to store characters
-    var charList = [];
-
-    // Iterate over each character in the string
-    for (var i = 0; i < str.length; i++) {
-        // Check if the character is not a space
-        if (str[i] !== ' ') {
-            // Add the character to the list
-            charList.push(str[i]);
-        }
-    }
-
-    // Return the list of characters
-    return charList;
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Reverse the cleaned string
+    const reversedStr = cleanStr.split('').reverse().join('');
+    
+    // Check if the cleaned string is equal to its reversed version
+    return cleanStr === reversedStr;
 }
 
 // Do not edit below this line
